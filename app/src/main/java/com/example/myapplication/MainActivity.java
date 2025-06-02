f964f3cf1f58f84de4464591022abca34177b28f
Levon Nazinyan <nazinyanlevon@gmail.com>
2025-06-02 17:14:48 +0400

    Fragment Added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -11,6 +14,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+import com.google.android.material.navigation.NavigationBarView;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -37,14 +42,37 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         loadFragment(new FragmentHome()); // Default fragment
 
+        bottomNavigationView.setItemActiveIndicatorEnabled(false);
+        //bottomNavigationView.setLabelVisibilityMode(NavigationBarView.LABEL_VISIBILITY_UNLABELED);
+
         bottomNavigationView.setOnItemSelectedListener(item -> {
             Fragment fragment = null;
             int id = item.getItemId();
+
+            bottomNavigationView.post(() -> {
+                final View view = bottomNavigationView.findViewById(item.getItemId());
+                if (view != null) {
+                    view.animate()
+                            .scaleX(1.2f)
+                            .scaleY(1.2f)
+                            .setDuration(150)
+                            .withEndAction(() -> {
+                                view.animate().scaleX(1f).scaleY(1f).setDuration(150).start();
+                            }).start();
+                }
+            });
+
 
             if (id == R.id.nav_home) {
                 fragment = new FragmentHome();
             } else if (id == R.id.nav_profile) {
                 fragment = new FragmentProfile();
+            } else if (id == R.id.nav_add) {
+                fragment = new FragmentAdd();
+            } else if (id == R.id.nav_order) {
+                fragment = new FragmentOrders();
+            } else if (id == R.id.nav_wish) {
+                fragment = new FragmentWishlist();
             }
 
             if (fragment != null) {
@@ -55,12 +83,19 @@ public class MainActivity extends AppCompatActivity {
             return false;
         });
 
+
+
     }
 
     private void loadFragment(Fragment fragment) {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .commit();
+    }
+
+    private int dpToPx(int dp) {
+        float density = getResources().getDisplayMetrics().density;
+        return Math.round((float) dp * density);
     }
 
 
